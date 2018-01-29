60e1ca86fccf85bce016e787d2a847b985ba4527
dcharua <danielcharua@hotmail.com>
2018-01-29 14:19:04 -0600

    first commit

---
+++ client/imports/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Route } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+
+
+
+export const routes: Route[] = [
+	{ path: '', component: HomeComponent },
+	{ path: '**', component: HomeComponent }
+];
